be65527bb697b3feba0687196617811283fca0a7
dude <you@example.com>
2021-11-08 03:03:00 +0100

    WIP

--- src/config.ts
+++ src/config.ts
@@ -1,16 +1,30 @@
-import { AdminApi, Configuration } from '@oryd/hydra-client'
+import {AdminApi, Configuration} from '@oryd/hydra-client'
+import axios from "axios";
 
 const baseOptions: any = {}
 
 if (process.env.MOCK_TLS_TERMINATION) {
-  baseOptions.headers = { 'X-Forwarded-Proto': 'https' }
+    baseOptions.headers = {'X-Forwarded-Proto': 'https'}
 }
 
 const hydraAdmin = new AdminApi(
-  new Configuration({
-    basePath: process.env.HYDRA_ADMIN_URL,
-    baseOptions
-  })
+    new Configuration({
+        basePath: process.env.HYDRA_ADMIN_URL,
+        baseOptions
+    })
 )
 
-export { hydraAdmin }
+const vrsctest = axios.create({
+    baseURL: process.env.VRSCTEST_RPC_URL,
+    auth: {
+        username: process.env.VRSCTEST_RPC_USER || '',
+        password: process.env.VRSCTEST_RPC_PASSWORD || '',
+    }
+});
+
+const verusClient = {
+    vrsctest,
+    // add more verus pbaas chain clients here
+}
+
+export {hydraAdmin, verusClient}
